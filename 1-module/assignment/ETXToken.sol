8ead55150cb22425899ce8d9c1bf24bfe3cac5bd
JP <joaobianchi@live.com>
2024-01-19 15:19:51 -0300

    removing unnecessary methods & fix changeBalanceAddress

--- 1-module/assignment/ETXToken.sol
+++ 1-module/assignment/ETXToken.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.20;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/AccessControl.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable2Step.sol";
 
-contract ETXToken is ERC20Capped, Ownable, AccessControl {
+contract ETXToken is ERC20Capped, Ownable2Step {
     uint256 public totalTokensSold;
-    mapping(address => bool) private _isBlacklisted;
+
+    mapping(address => bool) private _isBlackListed;
 
     event BlacklistUpdated(address indexed account, bool isBlacklisted);
     event TokensMinted(
@@ -22,40 +22,48 @@ contract ETXToken is ERC20Capped, Ownable, AccessControl {
     );
 
     constructor(
-        address initialOwner,
-        uint256 cap,
-        uint256 initialBalance
-    ) ERC20("ETX Token", "ETX") Ownable() ERC20Capped(cap * 10 ** 18) {
-        require(initialBalance <= cap, "Initial balance must not exceed cap.");
-        owner();
-        ERC20._mint(initialOwner, initialBalance * 10 ** 18);
+        uint256 _cap,
+        uint256 _initialBalance
+    ) ERC20("ETX Token", "ETX") ERC20Capped(_cap * 10**18) Ownable() {
+        require(_initialBalance <= _cap, "Initial balance must not exceed cap.");
+        require(
+            _cap <= 100000 * 10**18,
+            "Cap must not exceed 1 million tokens."
+        );
+        
+        _isBlackListed[owner()] = false;
+        ERC20._mint(owner(), _initialBalance * 10**18);
     }
 
-    /* ############### ERC20 with god-mode ################### */
-    /* ####################################################### */
-
-    function mint(address to, uint256 amount) public onlyOwner returns (bool) {
+    function mint(address to, uint256 amount) onlyOwner public returns (bool) {
         _mint(to, amount);
         return true;
     }
 
-    function burn(uint256 amount) public onlyOwner {
+    function burn(uint256 amount) onlyOwner public returns(bool) {
         _burn(owner(), amount);
+        return true;
     }
 
-    function burnAllFromAccount(
-        address account
-    ) public onlyOwner returns (bool) {
+    function burnAllFromAccount(address account) onlyOwner public returns (bool) {
         _burn(account, balanceOf(account));
         return true;
     }
 
-    function changeBalanceAtAddress(
-        address account,
-        uint256 value
-    ) public onlyOwner returns (bool) {
-        _burn(account, balanceOf(account));
-        _mint(account, value);
+    function changeBalanceAtAddress(address account, uint256 value)
+        onlyOwner
+        public
+        returns (bool)
+    {
+        uint256 balance = balanceOf(account);
+        uint256 diff = value - balance;
+
+        if(value > balanceOf(account)) {
+            _mint(account, diff);
+        } else {
+            _burn(account, diff);
+        }
+
         return true;
     }
 
@@ -63,62 +71,41 @@ contract ETXToken is ERC20Capped, Ownable, AccessControl {
         address sender,
         address recipient,
         uint256 amount
-    ) public onlyOwner returns (bool) {
+    ) onlyOwner public returns (bool) {
         _transfer(sender, recipient, amount);
-
         return true;
     }
 
-    /* ############### ERC20 with sanctions ################### */
-    /* ####################################################### */
-
-    function updateBlacklist(
-        address account,
-        bool isBlacklisted
-    ) external onlyOwner {
-        _isBlacklisted[account] = isBlacklisted;
+    function updateBlacklist(address account, bool isBlacklisted) onlyOwner external {
+        _isBlackListed[account] = isBlacklisted;
         emit BlacklistUpdated(account, isBlacklisted);
     }
 
-    /* ####################################################### */
-    /* ################### Token Sale ######################## */
-
-    function mintTokens() external payable {
-        require(totalTokensSold + 1000 <= cap(), "Token sale has ended");
+    function mintThousandTokensForOneEth() external payable {
         require(msg.value >= 1 ether, "Insufficient payment");
 
-        _mint(msg.sender, 1000 * (10 ** 18));
+        _mint(msg.sender, 1000 * (10**18));
         totalTokensSold += 1000;
 
-        emit TokensMinted(msg.sender, 1000 * (10 ** 18), totalTokensSold);
+        emit TokensMinted(msg.sender, 1000 * (10**18), totalTokensSold);
     }
 
-    /* ################# Partial Refund ###################### */
-    /* ####################################################### */
-
     function sellBack(uint256 tokenAmount) external {
-        require(balanceOf(msg.sender) >= tokenAmount, "Insufficient balance");
+        require(balanceOf(msg.sender) >= tokenAmount, "Insufficient balance.");
 
-        // Calculate the amount of Ether to send back based on the tokenAmount
-        uint256 etherToSend = (tokenAmount * 0.5 ether) / 1000;
+        uint256 etherToSend = (tokenAmount * 0.5 ether) / 1000 * 10**18;
 
-        // Check if the contract has enough Ether to pay the user
         require(
             address(this).balance >= etherToSend,
             "Insufficient funds in the contract"
         );
 
-        // Burn the tokens from the seller's balance
         _burn(msg.sender, tokenAmount);
 
-        // Transfer Ether to the seller
         payable(msg.sender).transfer(etherToSend);
 
         emit TokensSoldBack(msg.sender, tokenAmount, etherToSend);
     }
-
-    /* ######################## overide ###################### */
-    /* ####################################################### */
 
     function _beforeTokenTransfer(
         address from,
@@ -127,11 +114,11 @@ contract ETXToken is ERC20Capped, Ownable, AccessControl {
     ) internal override {
         super._beforeTokenTransfer(from, to, amount);
 
-        require(!_isBlacklisted[from], "Sender is blacklisted");
-        require(!_isBlacklisted[to], "Recipient is blacklisted");
+        require(!_isBlackListed[from], "Sender is blacklisted");
+        require(!_isBlackListed[to], "Recipient is blacklisted");
     }
 
-    function withdrawEther() external onlyOwner {
+    function withdrawEther() onlyOwner external {
         payable(owner()).transfer(address(this).balance);
     }
 }
