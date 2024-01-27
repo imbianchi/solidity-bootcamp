81f47f33d9860e1bece74a301ee569af5d45b6bb
JP <joaobianchi@live.com>
2024-01-27 14:10:14 -0300

    :construction: isntall ethers - module 3

---
+++ 3-module/ethers-practice/2_read_smart_contract.js
@@ -0,0 +1,27 @@
+const { ethers } = require('ethers');
+
+const INFURA_ID = '';
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+
+const ERC20_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+];
+
+const address = '';
+const contract = new ethers.Contract(address, ERC20_ABI, provider);
+
+const main = async () => {
+    const name = await contract.name();
+    const symbol = await contract.symbol();
+    const totalSupply = await contract.totalSupply();
+
+    console.log(`\nReading from ${address}\n`);
+    console.log(`\Name: ${name}\n`);
+    console.log(`\nSymbol: ${symbol}\n`);
+    console.log(`\nTotal Supply: ${totalSupply}\n`);
+};
+
+main();
